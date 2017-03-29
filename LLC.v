02c1bcf2c5264076b899d65904bf8588fd11b4dc
Mohammad-Ali AʼRÄBI <aerabi@gmx.de>
2017-03-29 02:40:18 +0200

    Introduced ctx_contains and advanced a bit using it in the proof of exchange

--- LLC.v
+++ LLC.v
@@ -113,6 +113,61 @@ Proof.
   - intros i' T. simpl. rewrite -> concat_ctx_update. rewrite -> IHG. reflexivity.
 Qed.
 
+Example concat_ctx_update_lr : forall G1 G2 x T,
+  (update_ctx G1 x T) o G2 = G1 o (update_l_ctx x T G2).
+Proof. reflexivity. Qed.
+
+Proposition concat_ctx_assoc : forall G1 G2 G3, (G1 o G2) o G3 = G1 o (G2 o G3).
+Proof.
+  intros. admit.
+Qed.
+
+Fixpoint ctx_contains (G : ctx) (i : id) (T : ty) : Prop :=
+  match G with
+  | [] => False
+  | update_ctx G' i' T' => ((i = i') /\ (T = T')) \/ (ctx_contains G' i T)
+  end.
+
+Example ctx_contains_test_1 : forall x T, ctx_contains (update_ctx ([]) x T) x T.
+Proof. intros. simpl. left. split; reflexivity. Qed.
+
+Example ctx_contains_test_2 : forall G x T, ctx_contains (update_ctx G x T) x T.
+Proof. intros. simpl. left. split; reflexivity. Qed.
+
+Proposition ctx_contains_concat : forall G1 G2 x T, 
+  ctx_contains ((update_ctx G1 x T) o G2) x T.
+Proof.
+  intros. induction G2.
+  - simpl. rewrite -> concat_ctx_singleton. simpl. left. split; reflexivity.
+  - rewrite -> concat_ctx_update. simpl. right. simpl in IHG2. apply IHG2.
+Qed.
+
+Proposition ctx_contains_concat' : 
+  forall G x T, ctx_contains G x T ->
+  exists G1 G2, G = (update_ctx G1 x T) o G2.
+Proof.
+  intros. induction G.
+  - inversion H.
+  - simpl in H. inversion H.
+    + inversion H0. subst. exists G. exists ([]). simpl. rewrite -> concat_ctx_singleton. reflexivity.
+    + apply IHG in H0. inversion H0 as [G']. inversion H1 as [G''].
+      exists G'. exists (update_ctx G'' i t). rewrite -> H2. rewrite -> concat_ctx_update. reflexivity.
+Qed.
+
+Proposition ctx_contains_concat'' : forall G1 G2 x T x' T',
+  ctx_contains ( (update_ctx G1 x' T') o G2 ) x T ->
+  (ctx_contains G1 x T) \/ (x = x' /\ T = T') \/ (ctx_contains G2 x T).
+Proof.
+  intros. induction G2.
+  - simpl in H. rewrite -> concat_ctx_singleton in H. simpl in H. inversion H.
+    + right. left. apply H0.
+    + left. apply H0.
+  - rewrite -> concat_ctx_update in H. simpl in H. inversion H.
+    + right. right. simpl. left. apply H0.
+    + simpl in IHG2. apply IHG2 in H0. inversion H0; try (left; apply H1).
+      inversion H1; try (right; left; apply H2). right. right. simpl. right. apply H2.
+Qed.
+
 Inductive ctx_split : ctx -> ctx -> ctx -> Prop :=
   | M_Empty : ctx_split empty_ctx empty_ctx empty_ctx
   | M_Un : forall G G1 G2 x P,
@@ -229,45 +284,22 @@ where "G '|-' t '|' T" := (ctx_ty G t T).
 
 Hint Constructors ctx_ty.
 
-Proposition exchange_weak : forall t x1 x2 T1 T2 T,
-  update_ctx (update_ctx ([]) x1 T1) x2 T2 |- t | T ->
-  update_ctx (update_ctx ([]) x2 T2) x1 T1 |- t | T.
-Proof.
-  induction t.
-  - intros. inversion H. subst. induction G2.
-    + simpl in H0. rewrite -> concat_ctx_update in H0. apply update_ctx_unique in H0. inversion H0.
-      inversion H3. subst.
-      assert ( H' : update_ctx (update_ctx ([]) x2 T2) x1 T1 = (update_ctx ([]) x2 T2) o (update_ctx ([]) x1 T1) ).
-      { simpl. reflexivity. }
-      rewrite -> H'. apply T_Var. apply q_rel''_concat_ctx; try apply Q_Rel_Ctx_Empty. apply Q_Rel_Ctx_Update.
-      rewrite -> H1 in H2. inversion H2. subst. apply H7. apply Q_Rel_Ctx_Empty.
-    + simpl in H0. rewrite -> concat_ctx_update in H0. apply update_ctx_unique in H0. inversion H0.
-      inversion H3. subst. induction G2.
-      * simpl in H1. rewrite -> concat_ctx_update in H1. apply update_ctx_unique in H1.
-        inversion H1. inversion H5. subst.
-        assert ( H' : update_ctx (update_ctx ([]) x2 T2) x1 T1 = (update_ctx (update_ctx ([]) x2 T2) x1 T1) o ([]) ).
-        { rewrite -> concat_ctx_null_r. reflexivity. }
-        rewrite -> H'. apply T_Var. apply q_rel''_concat_ctx; try apply Q_Rel_Ctx_Empty.
-        rewrite -> concat_ctx_null_r in H4. subst. simpl in H2. apply H2.
-      * simpl in H1. rewrite -> concat_ctx_update in H1. apply update_ctx_unique in H1.
-        inversion H1. induction G2. simpl in H4. rewrite -> concat_ctx_update in H4. inversion H4.
-        simpl in H4. rewrite -> concat_ctx_update in H4. inversion H4.
-  - intros. inversion H. subst. apply T_Bool.
-    assert ( H' : update_ctx (update_ctx ([]) x1 T1) x2 T2 = (update_ctx ([]) x1 T1) o (update_ctx ([]) x2 T2) ).
-    { simpl. reflexivity. }
-    rewrite -> H' in H4. apply q_rel''_concat_ctx' in H4.
-    assert ( H'' : update_ctx (update_ctx ([]) x2 T2) x1 T1 = (update_ctx ([]) x2 T2) o (update_ctx ([]) x1 T1) ).
-    { simpl. reflexivity. }
-    rewrite -> H''. inversion H4. apply q_rel''_concat_ctx; auto.
-  - intros. inversion H. subst.
-  Admitted.
-
 Lemma exchange : forall t x1 x2 T1 T2 T G1 G2,
   concat_ctx (update_ctx (update_ctx G1 x1 T1) x2 T2) G2 |- t | T ->
   concat_ctx (update_ctx (update_ctx G1 x2 T2) x1 T1) G2 |- t | T.
 Proof.
   induction t.
-  - intros. inversion H. subst. admit.
+  - intros. inversion H. subst.
+    assert (Hc : ctx_contains ( (update_ctx G0 i T) o G3 ) i T).
+    { apply ctx_contains_concat. }
+    simpl in Hc. rewrite -> H0 in Hc. rewrite <- concat_ctx_update_lr in Hc. 
+    apply ctx_contains_concat'' in Hc. inversion Hc.
+    { apply ctx_contains_concat' in H1. inversion H1 as [G']. inversion H3 as [G'']. subst.
+      rewrite -> concat_ctx_update_lr. rewrite -> concat_ctx_update_lr. rewrite -> concat_ctx_assoc.
+      apply T_Var.
+    
+    apply ctx_contains_concat' in Hc. 
+    inversion Hc as [G']. inversion H1 as [G'']. simpl. 
   - admit.
   - intros. inversion H. subst.
 Qed.
