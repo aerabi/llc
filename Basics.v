54e481b3e97ce4b921f4cbf573efa37dd5d1d6c1
Mohammad-Ali AʼRÄBI <aerabi@gmx.de>
2017-05-03 13:20:20 +0200

    Define until the first lemma in Operational Semantics

--- Basics.v
+++ Basics.v
@@ -35,3 +35,48 @@ Inductive tm : Type :=
   | tmsplit : tm -> id -> id -> tm -> tm
   | tmabs : q -> id -> ty -> tm -> tm
   | tmapp : tm -> tm -> tm.
+
+(* Term Variable Replacement *)
+Fixpoint nat_eq ( m : nat ) ( n : nat ) : bool :=
+  match m, n with
+  | O, O => true
+  | S _, O => false
+  | O, S _ => false
+  | S m', S n' => nat_eq m' n'
+  end.
+
+Definition var_eq ( x : id ) ( y : id ) : bool :=
+  match x, y with
+  | Id m, Id n => nat_eq m n
+  end.
+
+Definition rpv ( x' : id ) ( x : id ) ( y : id ) : id :=
+  if var_eq x x' then y else x'.
+
+Fixpoint rp ( t : tm ) ( x : id ) ( y : id ) : tm :=
+  match t with
+  | tmvar x' => tmvar (rpv x' x y)
+  | tmbool qi bi => tmbool qi bi
+  | tmif t1 t2 t3 => tmif (rp t1 x y) (rp t2 x y) (rp t3 x y)
+  | tmpair qi t1 t2 => tmpair qi (rp t1 x y) (rp t2 x y)
+  | tmsplit t1 x1 x2 t2 => tmsplit (rp t1 x y) (rpv x1 x y) (rpv x2 x y) (rp t2 x y)
+  | tmabs qi x' T t => tmabs qi (rpv x' x y) T (rp t x y)
+  | tmapp t1 t2 => tmapp (rp t1 x y) (rp t2 x y)
+  end.
+
+(* Values and PreValues *)
+Reserved Notation "'pv'" (at level 10).
+
+Inductive v : Type :=
+  | pvbool : b -> pv
+  | pvpair : id -> id -> pv
+  | pvabs : id -> ty -> tm -> pv
+
+where "'pv'" := (q -> v).
+
+Fixpoint tmv (t : v) : tm :=
+  match t with
+  | pvbool bi qi => tmbool qi bi
+  | pvpair x y qi => tmpair qi (tmvar x) (tmvar y)
+  | pvabs x ti t' qi => tmabs qi x ti t'
+  end.
