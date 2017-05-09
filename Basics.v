df5f62d79b7fa71a1fbc542b17163ea902699c6a
Muhammad-Ali A'rabi <angellandros@yahoo.com>
2017-05-09 15:10:02 +0200

    Document Basics.v

--- Basics.v
+++ Basics.v
@@ -1,24 +1,24 @@
-(* Quantifiers : Linear & Unrestricted *)
+(* Quantifiers : Linear & Unrestricted [Figure 1-3, q] *)
 Inductive q : Type :=
-  | qlin : q
-  | qun : q.
+  | qlin : q  (* [Figure 1-3, lin] *)
+  | qun : q.  (* [Figure 1-3, un] *)
 
-(* Boolean Literal : True & False *)
+(* Boolean Literal : True & False [Figure 1-3, b] *)
 Inductive b : Type :=
-  | btrue : b
-  | bfalse : b.
+  | btrue : b    (* [Figure 1-3, true] *)
+  | bfalse : b.  (* [Figure 1-3, false] *)
 
-(* Names of the Variables *)
+(* Names of the Variables, [See Figure 1-3, x] *)
 Inductive id : Type :=
   | Id : nat -> id.
 
 (* Type and Pretype *)
-Reserved Notation "'qty'" (at level 10).
+Reserved Notation "'qty'" (at level 10).  (* [Figure 1-3, P] *)
 
-Inductive ty : Type :=
-  | ty_bool : qty
-  | ty_pair : ty -> ty -> qty
-  | ty_arrow : ty -> ty -> qty
+Inductive ty : Type :=  (* Figure 1-3, T] *)
+  | ty_bool : qty               (* [Figure 1-3, Bool] *)
+  | ty_pair : ty -> ty -> qty   (* [Figure 1-3, T*T] *)
+  | ty_arrow : ty -> ty -> qty  (* [Figure 1-3, T->T] *)
 
 where "'qty'" := (q -> ty).
 
@@ -26,15 +26,15 @@ Notation "T ** T'" := (ty_pair T T') (at level 20, left associativity).
 
 Notation "T --> T'" := (ty_arrow T T') (at level 40, left associativity).
 
-(* Terms *)
+(* Terms [Figure 1-3, t] *)
 Inductive tm : Type :=
-  | tmvar : id -> tm
-  | tmbool : q -> b -> tm
-  | tmif : tm -> tm -> tm -> tm
-  | tmpair : q -> tm -> tm -> tm
-  | tmsplit : tm -> id -> id -> tm -> tm
-  | tmabs : q -> id -> ty -> tm -> tm
-  | tmapp : tm -> tm -> tm.
+  | tmvar : id -> tm                       (* [Figure 1-3, x] *)
+  | tmbool : q -> b -> tm                  (* [Figure 1-3, q b] *)
+  | tmif : tm -> tm -> tm -> tm            (* [Figure 1-3, if t then t else t] *)
+  | tmpair : q -> tm -> tm -> tm           (* [Figure 1-3, q <t, t>] *)
+  | tmsplit : tm -> id -> id -> tm -> tm   (* [Figure 1-3, split t as x, y in t] *)
+  | tmabs : q -> id -> ty -> tm -> tm      (* [Figure 1-3, q λx:T.t] *)
+  | tmapp : tm -> tm -> tm.                (* [Figure 1-3, t t] *)
 
 (* Term Variable Replacement *)
 Fixpoint nat_eq ( m : nat ) ( n : nat ) : bool :=
@@ -50,10 +50,10 @@ Definition var_eq ( x : id ) ( y : id ) : bool :=
   | Id m, Id n => nat_eq m n
   end.
 
-Definition rpv ( x' : id ) ( x : id ) ( y : id ) : id :=
+Definition rpv ( x' : id ) ( x : id ) ( y : id ) : id :=  (* [x->y]x' *)
   if var_eq x x' then y else x'.
 
-Fixpoint rp ( t : tm ) ( x : id ) ( y : id ) : tm :=
+Fixpoint rp ( t : tm ) ( x : id ) ( y : id ) : tm :=  (* [x->y]t *)
   match t with
   | tmvar x' => tmvar (rpv x' x y)
   | tmbool qi bi => tmbool qi bi
@@ -65,12 +65,12 @@ Fixpoint rp ( t : tm ) ( x : id ) ( y : id ) : tm :=
   end.
 
 (* Values and PreValues *)
-Reserved Notation "'pv'" (at level 10).
+Reserved Notation "'pv'" (at level 10).  (* [Figure 1-7, w] *)
 
-Inductive v : Type :=
-  | pvbool : b -> pv
-  | pvpair : id -> id -> pv
-  | pvabs : id -> ty -> tm -> pv
+Inductive v : Type :=  (* [Figure 1-7, v] *)
+  | pvbool : b -> pv              (* [Figure 1-7, b] *)
+  | pvpair : id -> id -> pv       (* [Figure 1-7, <x, y>] *)
+  | pvabs : id -> ty -> tm -> pv  (* [Figure 1-7, λx:T.t] *)
 
 where "'pv'" := (q -> v).
 
